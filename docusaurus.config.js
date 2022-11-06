d0f0891a33a973744ca833f30f0f64961a44fe7f
Sebastian <moondedow@gmail.com>
2022-11-06 15:27:41 +0100

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,8 +15,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "RedModding", // Usually your GitHub org/user name.
-  projectName: "WolvenKit", // Usually your repo name.
+  organizationName: "WolvenKit", // Usually your GitHub org/user name.
+  projectName: "documentation", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
